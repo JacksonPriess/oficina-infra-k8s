4aad8d8bb0649340b91963a8f9b565d425f6e7c0
Jackson Priess JP <7551889+JacksonPriess@users.noreply.github.com>
2026-09-04 20:28:32 -0300

    Removendo dependencia do lambda do k8s.

--- terraform/auth-remote-state.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "auth_lambda" {
-  backend = "s3"
-
-  config = {
-    bucket = "oficina-state-priess951"
-    key    = "infra/auth-lambda/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
